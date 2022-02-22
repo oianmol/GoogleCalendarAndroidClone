67b15664baf532d1e6200ef682befda546194a32
Anmol Verma <anmol.verma4@gmail.com>
2022-02-22 15:14:17 +0530

    setup scaffold

--- ui-dashboard/src/main/java/dev/baseio/googlecalendar/uidashboard/ui/DashboardUI.kt
+++ ui-dashboard/src/main/java/dev/baseio/googlecalendar/uidashboard/ui/DashboardUI.kt
@@ -1,9 +1,39 @@
 package dev.baseio.googlecalendar.uidashboard.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.statusBarsPadding
+import dev.baseio.googlecalendar.commonui.theme.GoogleCalendarColorProvider
+import dev.baseio.googlecalendar.commonui.theme.GoogleCalendarSurface
+import dev.baseio.googlecalendar.commonui.theme.GoogleCalendarTheme
 import dev.baseio.googlecalendar.navigator.ComposeNavigator
 
 @Composable
 fun DashboardUI(composeNavigator: ComposeNavigator) {
+  GoogleCalendarTheme {
+    val scaffoldState = rememberScaffoldState()
 
+    Scaffold(
+      backgroundColor = GoogleCalendarColorProvider.colors.uiBackground,
+      contentColor = GoogleCalendarColorProvider.colors.textPrimary,
+      modifier = Modifier.statusBarsPadding(), scaffoldState = scaffoldState, snackbarHost = {
+        scaffoldState.snackbarHostState
+      }
+    ) { innerPadding ->
+      Box(modifier = Modifier.padding(innerPadding)) {
+        GoogleCalendarSurface(
+          modifier = Modifier
+            .padding(12.dp)
+        ) {
+
+        }
+      }
+    }
+  }
 }
